b53ede66a6f1b3edb302428f303ac320ba6ada03
nsn <sainagaakhil@gmail.com>
2020-06-10 18:04:05 -0400

    first commit

---
+++ demoMongoDB/src/main/java/com/example/demo/repository/PersonRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.model.Person;
+
+@Repository
+public interface PersonRepository extends MongoRepository<Person, String> {
+	public Person findByFirstName(String firstName);
+	//public Person findByLastName(String lastName);
+	public List<Person> findByAge(int age);
+	
+}
